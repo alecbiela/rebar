3db3b28f50bc12762da874b7abe5fcd089cc75eb
alecbiela <alec.bielanos@gmail.com>
2024-06-17 08:29:44 -0400

    Remove quirks/almost standards mode from HTML declarations

--- web/concrete/themes/dark_chocolate/elements/header.php
+++ web/concrete/themes/dark_chocolate/elements/header.php
@@ -1,6 +1,5 @@
 <?php defined('C5_EXECUTE') or die("Access Denied."); ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 <html lang="<?php echo LANGUAGE?>" xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <?php Loader::element('header_required'); ?>
